fd257179177f85ec56949b9ff935e4aff24de8b4
opilane <opilane@HP-CZC5261P8F>
2022-09-28 11:33:24 +0300

    zxzxcczx

--- kolmerakendust/Form1.cs
+++ kolmerakendust/Form1.cs
@@ -15,15 +15,31 @@ namespace kolmerakendust
     public partial class Form1 : Form
     {
         TableLayoutPanel tableLayoutPanel;
+        FlowLayoutPanel flowLayoutPanel;
         Button showPicture;
         PictureBox pb;
         System.Windows.Forms.CheckBox cb;
-        ColorDialog colorDialog1 = new ColorDialog();
-        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        ColorDialog colorDialog;
+        OpenFileDialog openFileDialog;
 
 
         public Form1()
         {
+            this.Text = "Pildid";
+            this.Size = new System.Drawing.Size(800, 800);
+            openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "JPEG Files(*.jpg) | *.jpg | PNG Files(*.png) | *.png | BMP Files(*.bmp) | *.bmp | All files( *.*)";
+            colorDialog = new ColorDialog();
+            tableLayoutPanel = new TableLayoutPanel
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                AutoSize = true,
+                ColumnCount = 2,
+                RowCount = 2,
+                Location = new System.Drawing.Point(20, 20),
+                TabIndex = 0,
+                BackColor = System.Drawing.Color.White,
+            };
             tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
                 (System.Windows.Forms.SizeType.Percent, 15F));
@@ -33,16 +49,35 @@ namespace kolmerakendust
                 (System.Windows.Forms.SizeType.Percent, 90F));
             tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle
                 (System.Windows.Forms.SizeType.Percent, 10F));
+            flowLayoutPanel = new FlowLayoutPanel
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft,
+                WrapContents = false,
+                BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+            };
 
             Button showPicture = new Button
             {
-                Text = "Foto",
-                Size = new System.Drawing.Size(90, 80),
-                Location = new System.Drawing.Point(370, 90)
+                Text = "Näita foto",
             };
             showPicture.Click += showButton_Click;
-            tableLayoutPanel.Controls.Add(showPicture, 0, 0);
-
+            Button background = new Button
+            {
+                Text = "Määrake taustavärv",
+            };
+            background.Click += backgroundButton_Click;
+            Button close = new Button
+            {
+                Text = "Sulge",
+            };
+            close.Click += closeButton_Click;
+            Button clear = new Button
+            {
+                Text = "Tühjenda pilt",
+            };
+            clear.Click += clearButton_Click;
+            Button[] buttons = { showPicture, background, close, close };
 
             PictureBox pb = new PictureBox
             {
@@ -53,7 +88,16 @@ namespace kolmerakendust
                 AutoSize = true,
             };
 
-            cb = new CheckBox { };
+            cb = new CheckBox
+            {
+                Text = "Venitada",
+            };
+            cb.CheckedChanged += checkBox1_CheckedChanged;
+            tableLayoutPanel.Controls.Add(pb, 1, 0);
+
+            this.Controls.Add(tableLayoutPanel);
+           // tableLayoutPanel.Controls.Add(buttons, 1, 1);
+            tableLayoutPanel.Controls.Add(flowLayoutPanel, 1, 1);
             tableLayoutPanel.Controls.Add(pb, 1, 0);
         }
 
@@ -61,9 +105,9 @@ namespace kolmerakendust
         {
             // Show the Open File dialog. If the user clicks OK, load the
             // picture that the user chose.
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                pb.Load(openFileDialog1.FileName);
+                pb.Load(openFileDialog.FileName);
             }
         }
         private void clearButton_Click(object sender, EventArgs e)
@@ -76,8 +120,8 @@ namespace kolmerakendust
         {
             // Show the color dialog box. If the user clicks OK, change the
             // PictureBox control's background to the color the user chose.
-            if (colorDialog1.ShowDialog() == DialogResult.OK)
-                pb.BackColor = colorDialog1.Color;
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+                pb.BackColor = colorDialog.Color;
         }
 
         private void closeButton_Click(object sender, EventArgs e)
