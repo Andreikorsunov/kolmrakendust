91bcb54d8004c098c1ef119752f70eb3fde95f1a
AndreiKorsunov <andrei.korshunov2004@gmail.com>
2022-10-05 09:10:10 +0300

    added classes User, Login, Register

---
+++ kolmerakendust/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolmerakendust
+{
+    public class User
+    {
+        public int Id;
+        public string Knimi;
+        public string email;
+        public string sugu;
+        public int vanus;
+
+        public User() { }
+
+        /*public User(string Knimi, string email, string sugu, int vanus)
+        {
+            this.Knimi = Knimi;
+            this.email = email;
+            this.sugu = sugu;
+            this.vanus = vanus;
+        }*/
+    }
+}
